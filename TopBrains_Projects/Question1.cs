5381372143c57ed41af6ed083d10551cb6899a9e
Harsh Singh <117902266+Harshksingh1@users.noreply.github.com>
2026-01-19 12:21:20 +0530

    first

---
+++ TopBrains_Projects/Question1.cs
@@ -0,0 +1,36 @@
+using System;
+class program
+{
+    public static void Main(string[] args){
+    Console.WriteLine("Enter string 1");
+    string str1=Console.ReadLine();
+    Console.WriteLine("Enter string 2");
+    string str2=Console.ReadLine();
+    string result=" ";
+    HashSet<char> h = new HashSet<char>();
+    for(int i = 0; i < str2.Length; i++)
+        {
+            h.Add(str2[i]);
+        }
+    for(int i = 0; i < str1.Length; i++)
+        {
+            if((str1[i] !='a' && str1[i] !='e'&&str1[i] !='i'&&str1[i] !='o'&&str1[i] !='u' &&str1[i] !='A'&&str1[i] !='E'&&str1[i] !='I'&&str1[i] !='O'&&str1[i] != 'U') && h.Contains(str1[i]))
+            {
+                continue;
+            }
+            else
+            {
+                result+=str1[i];
+            }
+        }
+       
+     Console.WriteLine(result);
+     string result1=" ";
+     for(int i = 1; i < result.Length; i++)
+        {
+            if(result[i]!=result[i-1])
+            result1+=result[i];
+        }
+        Console.WriteLine(result1);
+}
+}
