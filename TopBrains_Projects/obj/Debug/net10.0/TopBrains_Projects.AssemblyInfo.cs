2527906dd172c16936319191593d86581019859e
Harsh Singh <117902266+Harshksingh1@users.noreply.github.com>
2026-04-11 15:16:42 +0530

    Calculator Assignment

--- TopBrains_Projects/obj/Debug/net10.0/TopBrains_Projects.AssemblyInfo.cs
+++ TopBrains_Projects/obj/Debug/net10.0/TopBrains_Projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopBrains_Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ec4c343c812ea7524cf672362f8130a6241bb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34f1364878f384d80739c27ddbb642e12483b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopBrains_Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopBrains_Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
